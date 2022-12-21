a8e5bd6ee901262a969193093566973e19b0e445
João Araujo <araujo_joaol@yahoo.com.br>
2022-12-21 07:54:08 -0300

    feat: Site Home html and css finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,30 @@
 @section('title', 'J EVENTS')
 @section('content')
 
-@foreach ($events as $event)
-    <p>{{$event->title}} -- {{$event->description}}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Find an Event that suits you</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Type...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <form action="">
+        <h2>Next Events</h2>
+        <p class="subtitle">See what events are coming up</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{$event->title}}">
+                    <div class="card-body">
+                        <p class="card-date">10/08/2022</p>
+                        <h5 class="card-title">{{$event->title}}</h5>
+                        <p class="card-participants">10 Participants</p>
+                        <a href="#" class="btn btn-primary">More...</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </form>
+</div>
 
 @endsection
